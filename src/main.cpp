bcc2e75514bc30e3569f2aa875e3790d89c36566
AndreasGBP <114382655+AndreasGBP@users.noreply.github.com>
2022-10-17 13:13:01 +0200

    Final edit

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,14 @@ extern "C"
 int main(void)
 {
    
-    double testcases[10] = {0.0, 0.3, 1.7, 2.4, 3.3, 3.1415, 5.8, 8.0, 11.0, 14.0};
+    double testcases[10] = {0.0, 0.3, 1.7, 2.4, 3.3, 3.1415, 5.8, 8.0, 11.0, 14.0}; //For every value above 10 the precision seems to go out the window
     for(int i = 0 ; i < 10 ; i++)
     {
-        printf("Test function: %lf\n", taylor_sine(testcases[i], 5));
-        printf("Built in sin function: %lf\n\n", sin(testcases[i]));
+        printf("Test function: %lf\n", taylor_sine(testcases[i], 5)); //Prints the result from my taylor_sine function
+        printf("Built in sin function: %lf\n\n", sin(testcases[i])); //Prints the result from the sine function built into math.h
         
     }
+    
     
 
 }
--- src/taylor_sine.c
+++ src/taylor_sine.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 
-double factorial(int x) //Factorial function makes to make the calculations easier
+double factorial(int x) //Factorial function to make the calculations easier
 {
     if(x == 0)
         return 1;
@@ -18,12 +18,12 @@ double taylor_sine(double x, int n)
     double result = 0.0;
 
     result = x - (pow(x,3)/factorial(3)); //Result for the first two terms
-    for(count; count <= n+5; count++) //Question for n terms
+    for(count; count <= n+5; count++) //Question for n terms. Here +5 is added for extra precision
     {
         if(count % 2 == 1)
-            result -= (pow(x,power)/factorial(power));
+            result -= (pow (x,power) / factorial(power));
         if(count % 2 == 0)
-            result += (pow(x,power)/factorial(power));
+            result += (pow (x,power) / factorial(power));
         power += 2;  
     }
     return result;
